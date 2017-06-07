4a97b637bdc4ca5bb597ff60ee671d2e460bf4e1
Richard Taylor <rjtaylor@qinetiq.com>
2017-06-07 15:59:22 +0100

    checkpoint

--- test_client.py
+++ test_client.py
@@ -1,6 +1,6 @@
 import asyncio
 import logging
-import sys
+import signal
 import session
 
 log = logging.getLogger(__name__)
@@ -16,46 +16,42 @@ async def r():
     while True:
         count += 1
         # print('Sending command ...')
-        await asyncio.sleep(0.001)
-        reply = await c.cmd(f'test_cmd {count}')
-        if (count % 1000) == 0:
-            print(f'cmd count {count}')
+        await asyncio.sleep(1)
+        try:
+            reply = await c.cmd(f'test_cmd {count}')
+        except TimeoutError:
+            log.warning(f"Timeout waiting for cmd: {cmd}")
+        # if (count % 1000) == 0:
+        print(f'cmd count {count}')
 
 
 async def update(msg):
-    if (msg % 10000) == 0:
+    if (msg % 100000) == 0:
         print(f'update: {msg}')
 
 
 c.register('test.topic', update)
 
 l = asyncio.get_event_loop()
-tasks = asyncio.wait([r(), c.process_update()])
+
+async def main():
+    results = await asyncio.gather(r(), c.process_update(), return_exceptions=True)
+    for e in results:
+        if isinstance(e, Exception) and not isinstance(e, asyncio.CancelledError):
+            logging.error("Exception thrown during shutdown", exc_info=(type(e), e, e.__traceback__))
+
+
+def stop(msg):
+    log.debug(msg)
+    tasks = asyncio.Task.all_tasks()
+    for task in tasks:
+        task.cancel()
+
+l.add_signal_handler(signal.SIGINT, stop, "Halting on SIGINT")
 
 try:
-    # Here `amain(loop)` is the core coroutine that may spawn any
-    # number of tasks
-    sys.exit(l.run_until_complete(tasks))
-except KeyboardInterrupt:
-    # Optionally show a message if the shutdown may take a while
-    print("Attempting graceful shutdown, press Ctrl+C again to exit…", flush=True)
-
-    # Do not show `asyncio.CancelledError` exceptions during shutdown
-    # (a lot of these may be generated, skip this if you prefer to see them)
-    def shutdown_exception_handler(l, context):
-        if ( "exception" not in context or not
-             isinstance(context["exception"], asyncio.CancelledError)):
-            l.default_exception_handler(context)
-    l.set_exception_handler(shutdown_exception_handler)
-
-    # Handle shutdown gracefully by waiting for all tasks to be cancelled
-    tasks = asyncio.gather(*asyncio.Task.all_tasks(loop=l), loop=l, return_exceptions=True)
-    tasks.add_done_callback(lambda t: loop.stop())
-    tasks.cancel()
-
-    # Keep the event loop running until it is either destroyed or all
-    # tasks have really terminated
-    while not tasks.done() and not l.is_closed():
-        l.run_forever()
+    l.run_until_complete(main())
+except asyncio.CancelledError:
+    log.debug('CancelledError')
 finally:
     l.close()
--- test_server.py
+++ test_server.py
@@ -1,6 +1,7 @@
 import asyncio
 import logging
 import session
+import signal
 
 log = logging.getLogger(__name__)
 logging.basicConfig(level=logging.DEBUG)
@@ -8,21 +9,44 @@ logging.basicConfig(level=logging.DEBUG)
 
 async def chat(s):
     count = 0
-    while True:
-        try:
+    try:
+        while True:
             count += 1
-            await asyncio.sleep(0.001)
+            await asyncio.sleep(0)
             await s.publish('test.topic', count)
-        except:
-            log.exception('', exc_info=True)
-            break
+            if (count % 100000) == 0:
+                print(f'publish msg count {count}')
+    except:
+        log.exception('', exc_info=True)
+
 
 
 s = session.ServerSession()
 
 s.install()
-s.start()
 
 l = asyncio.get_event_loop()
-l.run_until_complete(asyncio.wait([chat(s), s.process_cmd()]))
-# l.run_until_complete(asyncio.wait([s.process_cmd(), ]))
+l.run_until_complete(s.start())
+
+async def main():
+    results = await asyncio.gather(chat(s), s.process_cmd(), return_exceptions=True)
+    for e in results:
+        if isinstance(e, Exception) and not isinstance(e, asyncio.CancelledError):
+            logging.error("Exception thrown during shutdown", exc_info=(type(e), e, e.__traceback__))
+
+
+def stop(msg):
+    log.debug(msg)
+    tasks = asyncio.Task.all_tasks()
+    for task in tasks:
+        task.cancel()
+
+
+l.add_signal_handler(signal.SIGINT, stop, "Halting on SIGINT")
+
+try:
+    l.run_until_complete(main())
+except asyncio.CancelledError:
+    log.debug('CancelledError')
+finally:
+    l.close()
